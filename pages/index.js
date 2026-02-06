358b54e772fe22657f8ed5ce7a9241bd9916cee6
Augusto Lima <augusto.acql@gmail.com>
2026-02-06 12:58:35 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,53 @@
 function Home(){
-    return <h1>Corrigindo o envio</h1>
+    return <h1>Tenho muito orgulho da médica que você esta se tornando!
+        Muitas forças e bora pra cima!!
+        Pena de urubu, pena de galinha, se você já deu o...
+        .
+        .
+        .
+        .
+        .
+        .
+        .
+        .
+        .
+        .
+        .
+        .
+        .
+        .
+        .
+        .
+        .
+        .
+        .
+        .
+        .
+        .
+        .
+        .
+        .
+        .
+        .
+        .
+        .
+        .
+        .
+        .
+        .
+        .
+        .
+        .
+        .
+        .
+        .
+        .
+        .
+        .
+        bumbum da uma risadinha ! rs
+
+        Seu marido te ama! 💜
+    </h1>
 }
 
 export default Home;
