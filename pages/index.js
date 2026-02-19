30a9f823e50e2fde2adb10c909e0412827753873
Augusto Lima <augusto.acql@gmail.com>
2026-02-19 14:21:50 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,61 +1,73 @@
-function Home(){
-    return (
-        <h1>
-            Tenho muito orgulho da médica que você está se tornando!<br />
-            Muitas forças e bora pra cima!!<br />
-            Pena de urubu, pena de galinha, se você gostou do seu ...<br />
-            .<br />
-            .<br />
-            .<br />
-            .<br />
-            .<br />
-            .<br />
-            .<br />
-            .<br />
-            .<br />
-            .<br />
-            .<br />
-            .<br />
-            .<br />
-            .<br />
-            .<br />
-            .<br />
-            .<br />
-            .<br />
-            .<br />
-            .<br />
-            .<br />
-            .<br />
-            .<br />
-            .<br />         
-            .<br />
-            .<br />
-            .<br />
-            .<br />
-            .<br />
-            .<br />
-            .<br />
-            .<br />
-            .<br />
-            .<br />
-            .<br />
-            .<br />
-            .<br />
-            .<br />
-            .<br />
-            .<br />
-            .<br />
-            .<br />
-            .<br />
-            .<br />
-            .<br />
-            .<br />
-            .<br />
-            presente dá uma risadinha! rs<br />
-            <br />
-            Seu marido te ama! 💜
-        </h1>
-    );
+function Home() {
+  return (
+    <h1>
+      Tenho muito orgulho da médica que você está se tornando!
+      <br />
+      Muitas forças e bora pra cima!!
+      <br />
+      Pena de urubu, pena de galinha, se você gostou do seu ...
+      <br />
+      .<br />
+      .<br />
+      .<br />
+      .<br />
+      .<br />
+      .<br />
+      .<br />
+      .<br />
+      .<br />
+      .<br />
+      .<br />
+      .<br />
+      .<br />
+      .<br />
+      .<br />
+      .<br />
+      .<br />
+      .<br />
+      .<br />
+      .<br />
+      .<br />
+      .<br />
+      .<br />
+      .<br />
+      .<br />
+      .<br />
+      .<br />
+      .<br />
+      .<br />
+      .<br />
+      .<br />
+      .<br />
+      .<br />
+      .<br />
+      .<br />
+      .<br />
+      .<br />
+      .<br />
+      .<br />
+      .<br />
+      .<br />
+      .<br />
+      .<br />
+      .<br />
+      .<br />
+      .<br />
+      .<br />
+      presente dá uma risadinha! rs
+      <br />
+      <br />
+      Seu marido te ama! 💜
+    </h1>
+  );
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("indentação errada");
 }
 
 export default Home;
